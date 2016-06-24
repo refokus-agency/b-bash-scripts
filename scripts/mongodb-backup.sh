3036ab6ee4d3ff8418b02ef1433bf5c884a6dd95
Juan <beogip@gmail.com>
2016-06-24 15:25:04 -0300

    Added mongodb-backup.sh

---
+++ scripts/mongodb-backup.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+HOSTNAME=""
+DATABASE=""
+
+while [[ $# -gt 1 ]]
+do
+key="$1"
+
+case $key in
+    -h|--hostname)
+    HOSTNAME="$2"
+    shift # past argument
+    ;;
+    -d|--database)
+    DATABASE="$2"
+    shift # past argument
+    ;;
+    *)
+            # unknown option
+    ;;
+esac
+shift # past argument or value
+done
+
+if [ -z "$HOSTNAME" ]; then
+  echo "Please set a hostname using -h or --hostname"
+  exit
+fi
+if [ -z "$DATABASE" ]; then
+  echo "Please set a database using -d or --database"
+  exit
+fi
+
+DIR=`date +%y%m%d`
+DEST=/db_backups/$DIR
+mkdir $DEST
+mongodump -h $HOSTNAME -d $DATABASE -o $DEST
